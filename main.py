40bb678f7e359e54122d654d6a23d41847ea3e91
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-08-15 11:27:20 -0700

    Assignment description added

--- main.py
+++ main.py
@@ -2,3 +2,18 @@
 # STEM103 SUM2024
 # Rashi Goyal
 # Final Project: Social Equity Awareness Program
+"""
+Description: Develop a program that educates users about influential individuals 
+or events related to marginalized communities in history, such as Black, Indigenous, LGBT+. 
+Users can learn about notable figures, achievements, 
+or historical milestones through an interactive interface.
+Here's a great history database offered through the EvCC Library, Gale in Context, 
+US HistoryLinks to an external site. 
+If you access it off campus, you will may need log in information to use it.
+
+Requirements:
+1. Create a database or list of notable figures, events, or milestones related to history.
+2. Users can select a category (e.g., Black History, Native or Indigenous History, etc) and explore information.
+3. Implement a quiz feature that tests users' knowledge about the selected category.
+4. Make the program interactive and engaging
+"""
